c38d855d58ec4baace421caf7c54eb6769299ef2
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2022-04-21 08:21:40 +0200

    initial commit

--- plugin/src/main/java/de/intranda/goobi/plugins/SaveDocketFileStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/SaveDocketFileStepPlugin.java
@@ -36,10 +36,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleStepPlugin implements IStepPluginVersion2 {
+public class SaveDocketFileStepPlugin implements IStepPluginVersion2 {
     
     @Getter
-    private String title = "intranda_step_sample";
+    private String title = "intranda_step_save_docket_file";
     @Getter
     private Step step;
     @Getter
@@ -57,7 +57,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
         value = myconfig.getString("value", "default value"); 
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
-        log.info("Sample step plugin initialized");
+        log.info("SaveDocketFile step plugin initialized");
     }
 
     @Override
@@ -70,7 +70,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public String getPagePath() {
-        return "/uii/plugin_step_sample.xhtml";
+        return "/uii/plugin_step_save_docket_file.xhtml";
     }
 
     @Override
@@ -109,7 +109,7 @@ public class SampleStepPlugin implements IStepPluginVersion2 {
         boolean successful = true;
         // your logic goes here
         
-        log.info("Sample step plugin executed");
+        log.info("SaveDocketFile step plugin executed");
         if (!successful) {
             return PluginReturnValue.ERROR;
         }
